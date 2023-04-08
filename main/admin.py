b2718e41826034ea5c81d0994e95e4d1c33d06a0
sirdesmond09 <sirdesmond09@gmail.com>
2023-04-08 09:33:08 +0100

    add filter

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register([Message, EventPreference, LovedOneProfile, SMSResponse])
+admin.site.register([Message, EventPreference, LovedOneProfile])
+
+
+@admin.register(SMSResponse)
+class SMSResponseAdmin(admin.ModelAdmin):
+    list_display =  ["service", "timestamp"]
+    list_filter = ["timestamp"]
